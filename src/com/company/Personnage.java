6f3bb2a3a151ef4810fa01df36a382f3d69ca0e7
chkir med <chkirmedmed-ch@outlook.fr>
2018-09-16 16:02:15 +0000

    tout d'abord j'ai creé la classe personnage et FauteDeSaisie comme une classe des exeptions

---
+++ src/com/company/Personnage.java
@@ -0,0 +1,77 @@
+package com.company;
+
+public abstract class Personnage {
+    protected String nom;
+    protected int niveau;
+    protected int vitalite;
+    protected int force;
+    protected int agilite;
+    protected int intelligence;
+
+    public Personnage(String nom, int niveau, int force, int agilite, int intelligence) throws FauteDeSaisie {
+        this.nom = nom;
+        this.niveau = niveau;
+        this.force = force;
+        this.agilite = agilite;
+        this.intelligence = intelligence;
+        this.vitalite = niveau * 5;
+
+        if (force + agilite + intelligence > niveau)
+            throw new FauteDeSaisie();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getNomEtVitalite() {
+        return nom + " (" + vitalite + " Vitalité)";
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public int getVitalite() {
+        return vitalite;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public int getAgilite() {
+        return agilite;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+
+    public void infligeDommages(int dommage) {
+        this.vitalite -= dommage;
+        System.out.println(this.getNom() + " perd " + dommage + " points de vie") ;
+        if (this.vitalite <= 0)
+            System.out.println(this.getNom() + " est mort");
+    }
+
+
+    public abstract void attaqueBasique(Character cible);
+
+
+    public abstract void attaqueSpecial(Character cible);
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + nom + '\'' +
+                ", level=" + niveau +
+                ", vitality=" + vitalite +
+                ", strength=" + force +
+                ", agility=" + agilite +
+                ", intelligence=" + intelligence +
+                '}';
+    }
+}
+
